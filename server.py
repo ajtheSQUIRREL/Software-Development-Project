204503709dde20eaf69a501f4281f1533c2fa711
Abrar Jahin <abrar.j.official@gmail.com>
2024-02-19 12:53:10 +0600

    Getting data by calling API

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # Weather Application
 
 from geopy.geocoders import Nominatim
+import requests
+import json
 
 
 def weather_data():
@@ -15,7 +17,22 @@ def weather_data():
         Latitude = getLoc.latitude
         Longitude = getLoc.longitude
 
-        print(f"Latitude : {Latitude},Longitude : {Longitude}")
+        # print(f"Latitude : {Latitude},Longitude : {Longitude}")
+
+        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longitude}&appid=313c138faa75abd252dd9aefcd356d9c"
+
+        response = requests.get(api_url)
+        content = response.content.decode("utf-8")
+        dict_content = json.loads(content)
+        temperature = dict_content["main"]["temp"] - 273.16
+        print("Weather Update :")
+        print(f'Country        : {dict_content["sys"]["country"]}')
+        print(f"Place          : {location.capitalize()}")
+        print(f"Temperature    : {round(temperature,2)} Degree Celsius")
+        print(
+            f'Condition      : {dict_content["weather"][0]["description"].capitalize()}'
+        )
+        print(f'Humidity       : {dict_content["main"]["humidity"]}%')
 
     except:
         print("Sorry,You Entered Wrong Location.")
