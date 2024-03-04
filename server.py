ac71afe5508eb5ac443f4d278d2ca32611a8ea2e
Abrar Jahin <abrar.j.official@gmail.com>
2024-03-04 14:35:05 +0600

    Updated

--- server.py
+++ server.py
@@ -5,35 +5,31 @@ import requests
 import json
 
 
-def weather_data(lat, long):
-    # loc = Nominatim(user_agent="GetLoc")
+def weather_data(location=None):
+    loc = Nominatim(user_agent="GetLoc")
     # print("Enter Your Location : ", end=" ")
     # location = input()
 
-    # getLoc = loc.geocode(location)
-
+    getLoc = loc.geocode(location)
     try:
-
-        # Latitude = getLoc.latitude
-        # Longitude = getLoc.longitude
+        Latitude = getLoc.latitude
+        Longitude = getLoc.longitude
 
         # print(f"Latitude : {Latitude},Longitude : {Longitude}")
-
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid=313c138faa75abd252dd9aefcd356d9c"
-
+        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longitude}&appid=313c138faa75abd252dd9aefcd356d9c"
         response = requests.get(api_url)
-        content = response.content.decode("utf-8")
-        dict_content = json.loads(content)
+        dict_content = response.content.decode("utf-8")
+        # print(dict_content)
         temperature = dict_content["main"]["temp"] - 273.16
-        # print("Weather Update :")
-        # print(f'Country        : {dict_content["sys"]["country"]}')
+
+        # print(f'Country        : {dict_content["sys"]["country"]
         # print(f"Place          : {location.capitalize()}")
-        # print(f"Temperature    : {round(temperature,2)} Degree Celsius")
-        # print(
-        #     f'Condition      : {dict_content["weather"][0]["description"].capitalize()}'
-        # )
-        # print(f'Humidity       : {dict_content["main"]["humidity"]}%')
-        return temperature
+        Temperature = round(temperature, 2)
+        Condition = dict_content["weather"][0]["description"].capitalize()
+        Humidity = dict_content["main"]["humidity"]
+        pressure = dict_content["main"]["pressure"]
+        wind = dict_content["wind"]["speed"]
+        return Temperature, Condition, Humidity, pressure, wind
     except:
         print("Sorry,You Entered Wrong Location.")
 
